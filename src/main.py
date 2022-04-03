f7992e4722a5e32e56bcdcc01a6b2585d2d68e9a
KennyThrug <51040230+KennyThrug@users.noreply.github.com>
2022-04-03 01:05:57 -0500

    Add LCD screen

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import moveFiles
+import runLCDScreen
 import blinkLights
 import os
 if __name__ == '__main__':
-    blinkLights.blinkLights(3,24)
+    #blinkLights.blinkLights(3,24)
+    runLCDScreen.setupScreen()
