e2996ab0515bff28fbeadacec89b470f447aeb79
KennyThrug <kennyepstein6@gmail.com>
2022-04-08 13:02:18 -0500

    Makes button press only trigger once
    Unfortunately I had to add a small sleep timer -_- I hate inducing my own lag, but in the end is worth it so that I don't accidently do something dumb

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 import moveFiles
 import runGPIO
 import os
-
+from time import sleep
 def runLoop():
     buttonPressed = False #Quick boolean to make sure holding the button doesn't fuck things up because I know someone would do that :P
     runGPIO.setupButton(24)
     while(True):
-        if runGPIO.checkForButton(24) and not buttonPressed:
-            print("Find Directory of files")
-            print("MoveAllFiles") #TODO have each file move a loading bar on the LED
+        if (buttonPressed == False and runGPIO.checkForButton(24)):
             buttonPressed = True
+            print("Find Directory of files")
+            sleep(1)
+            print("MoveAllFiles") #TODO have each file move a loading bar on the LED
         else:
-            buttonPressed = False
+            if not runGPIO.checkForButton(24):
+                buttonPressed = False
 
 if __name__ == '__main__':
     runLoop()
