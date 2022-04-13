0e734eef60019f12d99facd90e037eea85119ec5
KennyThrug <51040230+KennyThrug@users.noreply.github.com>
2022-04-13 17:07:48 -0500

    Check for .HD file

--- src/FindDirectories.py
+++ src/FindDirectories.py
@@ -8,5 +8,40 @@ def getAllFiles(path):
             listofFiles.append((os.path.join(root,file))[len(path):])
     return listofFiles
 
+def mountAllDevices():
+    print("Todo")
+def unMountDevices():
+    print("Todo")
+
+def checkForHardDrive(path):
+    list = getAllFiles(path)
+    for x in list:
+        print(x)
+        if x == "/.HD":
+            print("found")
+            return True
+    return False
+
 def getBothPaths():
-    print("TODO")
+    mountAllDevices()
+
+    #Check to see which device is the Hardrive and which is the SD card
+    #HD will have a .HD File on the root directory
+    #If neither of them do, throw an error
+    dev1 = checkForHardDrive("dev1")
+    dev2 =  checkForHardDrive("dev2")
+    if(dev1 == dev2):
+        if(dev1):
+            print("ERR: Both dev's\r\nhave .HD file")
+        else:
+            print("ERR: No dev's\r\nhave .HD file")
+    else:
+        if(dev1):
+            print("dev1 has HD")
+        else:
+            print("dev2 has HD")
+        print("Do moving operation")
+    unMountDevices()
+
+if(__name__ == "__main__"):
+    getBothPaths()
